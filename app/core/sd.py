8abd9515fb8915c982e1b5df0ae1e396560428b7
ojitos369 <ojitos369@gmail.com>
2024-04-29 20:48:35 -0600

    aumentando el tiempo de espera de la peticion

--- app/core/sd.py
+++ app/core/sd.py
@@ -38,7 +38,7 @@ class StableDiffApi:
             },
             data=data,
         )
-        response = urllib.request.urlopen(request)
+        response = urllib.request.urlopen(request, timeout=600)
         return json.loads(response.read().decode('utf-8'))
 
     def call_txt2img_api(self, **payload):
